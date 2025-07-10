131956bc7b64fb8f79fd3b894edd64d94297c6b0
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-10 21:00:03 +0545

    password validator

---
+++ Week1/projects.py
@@ -0,0 +1,23 @@
+import string
+
+def strong_password(password):
+    if len(password)<8:
+        return False
+    elif not any(ch.isupper() for ch in password):
+        return False
+    elif not any(ch.islower() for ch in password):
+        return False
+    elif not any(ch.isdigit() for ch in password):
+        return False
+    elif not any(ch in string.punctuation for ch in password):
+        return False
+    else:
+        return True
+    
+while True:
+    password=input('Enter Password\n')
+    if strong_password(password):
+        print('Password is strong')
+        break
+    else:
+        print('Weak Password')
