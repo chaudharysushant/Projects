546d8744b1d73a4027d1f70d9c3f1686851d0c58
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-11 19:31:04 +0545

    hangman game

--- Week1/projects.py
+++ Week1/projects.py
@@ -1,19 +1,33 @@
-word = input("Enter the word: ")
+word = input("Enter the word: ").lower()
 cache = "_" * len(word)
 attempts = 6
 guessed_letters = []
 
+print("\n" * 50) 
+
 while attempts > 0 and cache != word:
     print("Current word:", cache)
-    guess = input("Guess a letter: ")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("You already guessed that letter. Try again.")
+        continue
+
     guessed_letters.append(guess)
     new_cache = ""
     for index, letter in enumerate(word):
-        print(f"{index}: {letter}")
+       
         if letter in guessed_letters:
             new_cache += letter
         else:
             new_cache += "_"
     cache = new_cache
+
     if guess not in word:
         attempts -= 1
+        print("Wrong Guess. Attempts left:", attempts)
+
+if cache == word:
+    print("You guessed the word:", word)
+else:
+    print("Game over! The word was:", word)
