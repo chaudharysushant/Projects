01a708e84ad9e130894afd0e93f26d99ca59e164
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-17 20:37:27 +0545

    Functions

--- projects.py
+++ projects.py
@@ -1,5 +1,2 @@
-sentence = "banana apple banana cherry apple banana"
-words = sentence.split()
-
-word_freq = {word: words.count(word) for word in set(words)}
-print(word_freq)
+square = lambda x:x*x
+print(square(5))
