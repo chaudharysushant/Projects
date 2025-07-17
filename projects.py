260322a027cec2d03cebbd6851de526820bbb477
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-17 21:07:48 +0545

    deduplicates

--- projects.py
+++ projects.py
@@ -1,5 +1,13 @@
-names = ['Asha', 'Bikash', 'Chitra']
-scores = [82, 90, 95]
 
-combine = list(zip(names, scores))
-print(combine)
+dups = [1,1,2,3,4,4,4,5,6,6,7,7,8]
+
+def deduplicate(dups):
+    seen = set()
+    result=[]
+    for item in dups:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+print(deduplicate(dups))
