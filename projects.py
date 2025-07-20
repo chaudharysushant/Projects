d4f01c5ed5116650a3b70790685a54b46d63db25
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-20 19:12:29 +0545

    modified

--- projects.py
+++ projects.py
@@ -2,7 +2,11 @@ class car:
     def __init__(self, brand, color):
         self.brand = brand
         self.color = color
+    
+    def drive(self):
+        print(f"{self.brand} is driving.")
 
 car1 = car("Toyota", "Red")
 print(car1.brand)
 print(car1.color)
+car1.drive()
