dfbe3a6c213badd4433817c5cf36314efa5c0a29
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-22 21:23:45 +0545

    practice test 1

--- projects.py
+++ projects.py
@@ -1,23 +1,7 @@
-class Shape:
-    def area(self):
-        pass
+class car:
+    def __init__(self, fuel):
+        self.__fuel = fuel
+    
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return 3.14 * self.radius * self.radius
-
-class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-shapes = [Circle(5), Rectangle(4, 6)]
-
-for shape in shapes:
-    print(f"Area: {shape.area()}")
+    def refuel(self,amount):
+        if amount > 0:
