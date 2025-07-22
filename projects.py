cd48602cd0b567e5314c8f530b71d83259355bc2
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-22 21:26:47 +0545

    changes

--- projects.py
+++ projects.py
@@ -1,7 +1,10 @@
-class car:
+class Car:
     def __init__(self, fuel):
         self.__fuel = fuel
     
-
-    def refuel(self,amount):
+    def refuel(self, amount):
         if amount > 0:
+            self.__fuel += amount
+            print(f"Fuel added: {amount}")
+        else:
+            print("Invalid fuel amount")
