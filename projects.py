42271981f15d3a30ab27375beb30522c8627dc3a
chaudharysushant <sushant.chaudary29@gmail.com>
2025-07-22 21:16:34 +0545

    encapsulation

--- projects.py
+++ projects.py
@@ -1,13 +1,25 @@
-class Animal:
+class student:
     def __init__(self, name):
         self.name = name
+        self._grades = []
+    
+    def add_grade(self, grade):
+        if 0 <= grade <= 100:
+            self._grades.append(grade)
+        
 
-    def speak(self):
-        print(f"{self.name} makes a sound")
+    def get_average(self):
+        if not self._grades:
+            return 0
+        return sum(self._grades)/len(self._grades)
+    
+    def get_grades(self):
+        return self._grades
+    
+s = student("ram")
+s.add_grade(90)
+s.add_grade(80)
 
-class Dog(Animal):
-    def speak(self):
-        print(f"{self.name} says woof")
-
-dog1 = Dog("Buddy")
-dog1.speak() 
+print(s.name)
+print(s.get_average())
+print(s.get_grades())
