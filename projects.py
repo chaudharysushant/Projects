5e2bda8deb9a345f6748459a3f414323aaacad9c
chaudarysushant <sushant.chaudary29@gmail.com>
2025-07-28 21:13:58 +0545

    view contacts

--- projects.py
+++ projects.py
@@ -1,20 +1,9 @@
 import csv
 
-file_path = r'C:\Users\Sushant-Chaudhary\Desktop\Projects\people_output.csv'
+FILE_PATH = r'C:\Users\Sushant-Chaudhary\Desktop\Projects\contacts.csv'
 
-# Data to write (a list of dictionaries)
-data = [
-    {'name': 'Sushant Chaudhary', 'email': 'sushant.chaudhary@example.com', 'isActive': True},
-    {'name': 'John Doe', 'email': 'john.doe@example.com', 'isActive': False}
-]
-
-with open(file_path, 'w', newline='') as file:
-    fieldnames = ['name', 'email', 'isActive']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-
-    # Write header
-    writer.writeheader()
-
-    # Write rows
-    for row in data:
-        writer.writerow(row)
+def view_contacts():
+with open(FILE_PATH, 'r') as file:
+    reader = csv.DictReader(file)
+    for contacts in reader:
+        print(f"Name: {contact['name']}, Email: {contact['email']}, Phone: {contact['phone']}")
